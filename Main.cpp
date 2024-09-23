d2704283723cb76023691cac2aa1b80e899dd70f
AlbinaBezchastnaya <Albina.Bezchastnaya@yandex.ru>
2024-09-23 12:08:30 +1000

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,71 @@
+﻿#include"Trie.h"
+#include <iostream>
+using namespace std;
+
+// За основу дерева взят код из модуля.
+// функция woodenAutoComplete будет заниматься выводом возможных вариантов слов 
+
+void test_woodenAutoComplete()
+{
+	TrieNode* root = getNewNode();
+	// тестовые строки и добавление в дерево
+	insert(root, "ab");
+	insert(root, "abde");
+	insert(root, "ac");
+	insert(root, "abcd");
+
+	insert(root, "bcd");
+	insert(root, "bd");
+	insert(root, "b");
+
+	//тест для поиска слов
+	std::string input = "a";
+	//string input = "ab";
+	//string input = "abd";
+	//string input = "ac";
+	//string input = "abdecd";
+
+	//string input = "b";
+
+	woodenAutoComplete(root, input, "");
+}
+
+
+int main()
+{
+	//test_woodenAutoComplete();
+
+	cout << " Hi! This is a simple program to complete words from the letters you enter." << endl;
+	cout << " Use only a-z keys." << endl;
+	cout << " Press q for exit." << endl;
+
+	TrieNode* root = getNewNode();
+
+	insert(root, "ab");
+	insert(root, "abde");
+	insert(root, "ac");
+	insert(root, "abcd");
+
+	insert(root, "bcd");
+	insert(root, "bd");
+	insert(root, "b");
+
+	bool work = true;
+	while (work)
+	{
+		cout << "Enter text: " << endl;
+
+		string strIn;
+		cin >> strIn;
+		if (strIn == "q")
+		{
+			work = false;
+			continue;
+		}
+		cout << " Word options:" << endl;
+		woodenAutoComplete(root, strIn, "");
+	}
+
+	delete root;
+	return 0;
+}
---
+++ Trie.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#define TRIE_H
+#include <string>
+#define ALPHABET_SIZE 26
+
+// Структура узела дерева 
+struct TrieNode
+{
+    struct TrieNode* children[ALPHABET_SIZE];
+    // isEndOfWord - true, если ключ является концом слова
+    bool isEndOfWord;
+    /* unsigned char letter;*/
+ //};
+
+};
+TrieNode* getNewNode(void);
+void insert(TrieNode*, std::string);
+bool search(TrieNode*, std::string);
+bool isEmpty(TrieNode*);
+TrieNode* remove(TrieNode*, std::string, int depth = 0);
+
+bool wordCompare(std::string& prefix, std::string& endWord);
+void woodenAutoComplete(TrieNode* root, std::string& prefix, std::string endWord);
+
